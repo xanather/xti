b640d6f2dc55204e29aceba63eb931a356d24424
Jordan Singh <me@jordansingh.com>
2025-03-07 19:57:43 +0800

    Big WIP

--- xti/main.cpp
+++ xti/main.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setStyle("Fusion");
+    a.setStyle("fusion");
     main_window w(nullptr);
     w.show();
     return a.exec();
