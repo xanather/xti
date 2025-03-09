655bcc8d62178b66df8e7de09a19c892dac884f2
Jordan Singh <me@jordansingh.com>
2025-03-09 12:49:39 +0800

    Woot woot

--- xti/main_window.cpp
+++ xti/main_window.cpp
@@ -31,6 +31,43 @@ main_window::main_window(QWidget *parent)
 
     setGeometry(0, screen->geometry().height() / 2 - 100, screen->geometry().width(), 200);
 
+    ui->comboBox_shortcutsAbove->addItem("Firefox");
+    ui->comboBox_shortcutsAbove->addItem("OneNote");
+    ui->comboBox_shortcutsAbove->addItem("Bitwarden");
+    ui->comboBox_shortcutsAbove->addItem("Yubico");
+    ui->comboBox_shortcutsAbove->addItem("Spotify");
+
+    ui->comboBox_shortcutsBelow->addItem("recrypt_gateway");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_core");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_account");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_auth");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_node");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_db");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_model");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_model_admin");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_model_internal");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_admin");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_client");
+
+    ui->comboBox_shortcutsBelow->addItem("system_monitor");
+    ui->comboBox_shortcutsBelow->addItem("ami_gen");
+    ui->comboBox_shortcutsBelow->addItem("v_common");
+    ui->comboBox_shortcutsBelow->addItem("aws_helpers");
+    ui->comboBox_shortcutsBelow->addItem("vpc");
+    ui->comboBox_shortcutsBelow->addItem("repository");
+    ui->comboBox_shortcutsBelow->addItem("secrets");
+    ui->comboBox_shortcutsBelow->addItem("dns_records");
+    ui->comboBox_shortcutsBelow->addItem("ephemeral");
+    ui->comboBox_shortcutsBelow->addItem("recrypt_template");
+    ui->comboBox_shortcutsBelow->addItem("enrichment_center");
+    ui->comboBox_shortcutsBelow->addItem("environment_manager");
+    ui->comboBox_shortcutsBelow->addItem("viron_site");
+    ui->comboBox_shortcutsBelow->addItem("vbalance");
+    ui->comboBox_shortcutsBelow->addItem("ci_script_generator");
+    ui->comboBox_shortcutsBelow->addItem("documentation");
+
+    ui->comboBox_shortcutsBelow->addItem("notepad++");
+
     connect(ui->pushButton_control, &QPushButton::clicked, this, &main_window::ui_on_control);
     connect(ui->pushButton_windows, &QPushButton::clicked, this, &main_window::ui_on_windows);
 }
