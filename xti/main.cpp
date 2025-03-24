2584250b1336e6307cbe669effce95de142469ad
Jordan Singh <me@jordansingh.com>
2025-03-24 19:50:41 +0800

    attempting to cursor position being fucked

--- xti/main.cpp
+++ xti/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     {
         error_reporter::stop(__FILE__, __LINE__, "Win32::CoInitializeEx() failure.");
     }
-    ::EnableMouseInPointer(FALSE);
+    ::EnableMouseInPointer(TRUE);
     QApplication a(argc, argv);
     a.setStyle("fusion");
     main_window w(nullptr);
