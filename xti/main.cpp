4012ce9ae081891e3c76fc353e80b0bbe5770a6e
Jordan Singh <me@jordansingh.com>
2025-03-30 12:19:47 +0800

    fine tuning

--- xti/main.cpp
+++ xti/main.cpp
@@ -30,6 +30,7 @@ int main(int argc, char *argv[])
     {
         error_reporter::stop(__FILE__, __LINE__, "Win32::CoInitializeEx() failure.");
     }
+    ::SetPriorityClass(::GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
     QApplication a(argc, argv);
     a.setStyle("fusion");
     main_window w(nullptr);
